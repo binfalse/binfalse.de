eb406102c9f995491e0087e97af933ecf5c9e570
Martin Scharm <martin@binfalse.de>
2015-01-28 22:57:04 +0100

    added url encoding functionality
    taken from http://blog.akciom.com/2013/01/11/setting-up-jekyll/

---
+++ _plugins/url_encode.rb
@@ -0,0 +1,15 @@
+# _plugins/url_encode.rb
+# taken from http://blog.akciom.com/2013/01/11/setting-up-jekyll/
+require 'liquid'
+require 'uri'
+
+# Percent encoding for URI conforming to RFC 3986.
+# Ref: http://tools.ietf.org/html/rfc3986#page-12
+module URLEncoding
+  def url_encode(url)
+    return URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+  end
+end
+
+Liquid::Template.register_filter(URLEncoding)
+
